f9adf1403aa4581e38e6c2e275dfdc5e0188047d
Yousef <syousef73@hotmail.com>
2020-01-16 11:29:14 +0100

    version 2
    en train de construir

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import oeuvre from './composants/oeuvre'
 
 export default () =>
   <div>
---
+++ src/composants/oeuvre.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const oeuvre = () => {
+
+    return (
+        <div>
+            <image onFocus= changepicture onClick=changepage />
+
+            <mytext></mytext>
+
+        </div>
+
+
+    )
+}
+
+const mytext () => {
+
+
+}
+
+
+export default  oeuvre;
